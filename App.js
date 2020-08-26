ed5aaed9be4775b7b2fc1c345f81bc7c7689f9f4
Nguyene <68597013+gwenz1235@users.noreply.github.com>
2020-08-26 14:28:06 +0700

    Nộp bài tập

--- App.js
+++ App.js
@@ -1,114 +1,251 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { View, Text, Image, Dimensions, ScrollView, TouchableOpacity } from 'react-native';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+import { rectangle,notification,privaty,Chat,data,devices,back } from './assets/images/index';
+
+
+const { width } = Dimensions.get('window')
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+
+    return (
+      <ScrollView style={{
+        marginTop: 74 ,
+        paddingLeft: 32,
+        paddingRight: 32,
+        fontFamily: "Gilroy-ExtraBold"
+
+      }}>
+        <TouchableOpacity
+        style={{
+          flexDirection: 'row',
+          alignItems:'center'
+        }}>
+      <View style={{ flexDirection: 'row', }}>
+      <Image
+        source={back}
+        
+        resizeMode="contain"
+      />
+
+
+      <View >
+        <Text style={{
+          fontSize: 25 /414* width,
+          fontWeight:'bold',
+          fontFamily: 'Gilroy-ExtraBold',
+          color: '#2675EC',
+
+
+        }}>  @wdlam </Text>
+        
+
+        <View style={{ flexDirection: 'row', alignItems: 'center',  }}>
+          <Image
+            source={rectangle}
+            style={{
+              width: 77/414 * width,
+              height: 77/414 * width,
+            }}
+            resizeMode="contain"
+          />
+
+          <View style={{ flexDirection: 'column', paddingHorizontal: 18 }}>
+            <Text
+              style={{
+                textAlign: 'left',
+                fontSize: 23/414 * width,
+                fontWeight: "bold",
+                fontFamily: 'Gilroy',
+                color: '#131313'
+              }}
+            >Gloria Mckinney</Text>
+
+
+            <View style={{ flexDirection: 'column' }}>
+              <Text
+                style={{
+                  textAlign: 'left',
+                  fontSize: 17/414 * width,
+                  fontWeight: 'bold',
+                  fontFamily: 'Gilroy',
+                  letterSpacing: 1.335/414 * width,
+                  color: '#848484'
+                }}>+375(29)9638433</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        </View>
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
-export default App;
+        <View style={{ flexDirection: 'column',  }}>
+          <Text
+            style={{
+              fontFamily: 'Gilroy',
+              fontSize: 20/414 * width,
+              fontWeight: 'bold',
+              textAlign: 'left',
+              top: 25/414 * width,
+              color: '#131313'
+
+            }}>Account</Text>
+
+          <View style={{ flexDirection: 'column', paddingVertical: 30 }}>
+            <Text style={{
+              textAlign: 'left',
+              fontFamily: 'Gilroy',
+              fontWeight: 'bold',
+              fontSize: 17/414 * width
+            }}>+375(29)9638433</Text>
+
+
+            <View style={{ flexDirection: 'column', paddingVertical: 13 }} >
+              <Text style={{
+                textAlign: 'left',
+                fontFamily: 'Gilroy',
+                fontWeight: '600',
+                fontSize: 17/414 * width,
+                lineHeight: 21/414 * width,
+                color: '#848484'
+
+              }}>
+                Tap to change phone number
+              </Text>
+
+              <View
+                style={{
+                  flexDirection: 'column',
+                  paddingVertical: 8/414 * width,
+                  borderBottomColor: '#F6F6F6',
+                  borderBottomWidth: 2/414 * width,
+                }}
+              />
+              <View style={{ flexDirection: 'column', paddingVertical: 13 }}>
+                <Text style={{
+                  textAlign: 'left',
+                  fontFamily: 'Gilroy-ExtraBold',
+                  fontWeight: 'bold',
+                  fontSize: 17/414 * width,
+                  color: '#131313'
+                }}>@wdlam</Text>
+
+                <View style={{ flexDirection: 'column', paddingVertical: 6 }}>
+                  <Text style={{
+                    textAlign: 'left',
+                    fontFamily: 'Gilroy',
+                    fontWeight: '600',
+                    fontSize: 17/414 * width,
+                    color: '#848484'
+                  }}>Username</Text>
+
+
+                  <View
+                    style={{
+                      flexDirection: 'column',
+                      paddingVertical: 6/414 * width,
+                      borderBottomColor: '#F6F6F6',
+                      borderBottomWidth: 2/414 * width,
+                    }}
+                  />
+                  <View style={{ flexDirection: 'column', paddingVertical: 25 }}>
+                    <Text style={{
+                      textAlign: 'left',
+                      fontFamily: 'Gilroy',
+                      fontWeight: 'bold',
+                      fontSize: 17/414 * width,
+                      color: '#131313'
+                    }}>I'm Senior Frontend Developer from Microsoft</Text>
+
+                    <View style={{ flexDirection: 'column', paddingVertical: 6 }}>
+                      <Text style={{
+                        textAlign: 'left',
+                        fontFamily: 'Gilroy',
+                        fontWeight: '600',
+                        fontSize: 17/414 * width,
+                        color: '#848484'
+                      }}>Bio</Text>
+
+                      <View style={{ flexDirection: 'column',paddingVertical: 33 }}>
+                        <Text
+                          style={{
+                            fontFamily: 'Gilroy',
+                            fontSize: 20/414 * width,
+                            fontWeight: 'bold',
+                            textAlign: 'left',
+                            top: 25/414 * width,
+                            color: '#131313'
+
+                          }}>Settings</Text>
+
+
+                        <View style={{ flexDirection: 'column', paddingVertical: 60, }}>
+                          <Image
+                            source={notification}
+                            resizeMode="contain"
+                          />
+                          <View style={{ flexDirection: 'column', paddingVertical: 31, }}>
+                            <Image
+                              source={privaty}
+                              resizeMode="contain"
+                            />
+                            <View style={{ flexDirection: 'column', paddingVertical: 31, }}>
+
+                              <Image
+                                source={data}
+
+                                resizeMode="contain"
+                              />
+                              <View style={{ flexDirection: 'column', paddingVertical: 31, }}>
+
+                                <Image
+                                  source={Chat}
+
+                                  resizeMode="contain"
+                                />
+                                <View style={{ flexDirection: 'column', paddingVertical: 31, }}>
+
+                                  <Image
+                                    source={devices}
+                                    resizeMode="contain"
+                                  />
+
+
+
+
+
+                                </View>
+                              </View>
+                            </View>
+                          </View>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+      </View>
+      </TouchableOpacity>
+
+      </ScrollView>
+
+
+
+
+
+
+
+
+
+    )
+  }
+}
+
+export default App
